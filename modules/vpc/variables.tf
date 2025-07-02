6e80adaf0e1ba194185e935af88340d0c2d4b1fa
Divyasaashi <158747902+Divyasaashi@users.noreply.github.com>
2025-07-02 17:27:28 +0530

    Create variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,12 @@
+variable "vpc_cidr" {
+  description = "vpc cidr"
+}
+variable "pub_cidr" {
+  description = "subnet cidr"
+}
+variable "pri_cidr" {
+  description = "private cidr"
+}
+variable "availability_zone" {
+  description = "availability_zone"
+}
