a0bf58abf54a93edca49aa00b01c34e92d583bfe
Divyasaashi <158747902+Divyasaashi@users.noreply.github.com>
2025-07-02 17:24:54 +0530

    Create variables.tf

---
+++ modules/ec2_instance/variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+description = "ami"
+}
+variable "instance_type" {
+description = "instance"
+}
+variable "security_groups" {
+description = "secuiryt"
+}
+variable "tags" {
+description = "tags"
+}
+variable "key_name" {
+description = "key_name"
+}
